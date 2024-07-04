04547d68f1c22897f7b106933ce1039077a87256
Carine Kuimi <kuimicarine41@gmail.com>
2024-07-04 19:40:23 +0200

    initial commit

---
+++ data_wangling.py
@@ -0,0 +1,26 @@
+import pandas as pd
+import matplotlib.pylab as plt
+
+from pyodide.http import pyfetch
+
+async def download(url, filename):
+    response = await pyfetch(url)
+    if response.status == 200:
+        with open(filename, "wb") as f:
+            f.write(await response.bytes())
+
+file_path="https://cf-courses-data.s3.us.cloud-object-storage.appdomain.cloud/IBMDeveloperSkillsNetwork-DA0101EN-SkillsNetwork/labs/Data%20files/auto.csv"
+
+#await 
+download (file_path, "usedcars.csv")
+
+file_name="usedcars.csv"
+
+headers = ["symboling","normalized-losses","make","fuel-type","aspiration", "num-of-doors","body-style",
+         "drive-wheels","engine-location","wheel-base", "length","width","height","curb-weight","engine-type",
+         "num-of-cylinders", "engine-size","fuel-system","bore","stroke","compression-ratio","horsepower",
+         "peak-rpm","city-mpg","highway-mpg","price"]
+df = pd.read_csv(filename, names = headers)
+
+df.head()
+print(df)
